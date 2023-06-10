19413f1e4718941443aab93e6377bbfe83826534
Perny <humans@perny.online>
2023-06-10 20:30:03 +0300

    Some more text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import Hero from "./Hero.svelte"
+    import Me from "./Me.svelte"
 </script>
 
 <Hero />
+<Me />
---
+++ src/routes/Me.svelte
@@ -0,0 +1,42 @@
+<div class="container">
+	<h1>About me</h1>
+	<p>
+		Crafting user friendly and aesthetic Ul designs is not just my profession, its my passion.
+		Taking a complex idea and bringing life to it through design, brings me a great sense of
+		satisfaction. I strive to make every design unique, with the user in mind, always aiming to add
+		value to the user experience.
+	</p>
+
+	<p>
+		I possess extensive expertise in <a href="/projects?tech=react">React</a> and the <a href="/projects?tech=nextjs">Next.js</a> framework. Recently, I ventured into
+		<a href="/projects?tech=svelte">Svelte</a> (alongside <a href="/projects?tech=svelte">SvelteKit</a>), and I must say, I am absolutely thrilled with it! Svelte has truly
+		captured my heart, surpassing my fondness for React. I am eagerly looking forward to
+		incorporating it more frequently in my future projects. Typically, my workflow involves crafting
+		the design in <a href="/projects?tech=figma">Figma</a> before delving into coding in VSCode, where I utilize <a href="/projects">Git</a> for version
+		control.
+	</p>
+</div>
+
+<style>
+	.container {
+		background-color: var(--bg-2);
+		padding-top: 3rem;
+		text-align: center;
+		padding-bottom: 3rem;
+	}
+	.container > h1 {
+		font-size: 3rem;
+	}
+	.container > p {
+		font-size: 1.5rem;
+		padding: 0 10rem;
+		max-width: 70vw;
+		margin: 0 auto;
+        margin-bottom: 2rem;
+	}
+
+    .container > p > a {
+        font-weight: bold;
+    }
+    
+</style>
